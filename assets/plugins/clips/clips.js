8baf73931dc774e7ec4f64a5778dd7d2e9d7443a
xiaomalover <xiaomalover@gmail.com>
2015-10-04 12:26:07 +0800

    Update clips.js

--- assets/plugins/clips/clips.js
+++ assets/plugins/clips/clips.js
@@ -7,7 +7,7 @@
 			{
 				var items = [
 					['Lorem ipsum...', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
-					['Red label', '<span class="label-red">Label</span>']
+					[this.lang.get('red_label'), '<span class="label-red">Label</span>']
 				];
 
 				this.clips.template = $('<ul id="redactor-modal-list">');
@@ -25,13 +25,13 @@
 
 				this.modal.addTemplate('clips', '<section>' + this.utils.getOuterHtml(this.clips.template) + '</section>');
 
-				var button = this.button.add('clips', 'Clips');
+				var button = this.button.add('clips', this.lang.get('clips'));
 				this.button.addCallback(button, this.clips.show);
 
 			},
 			show: function()
 			{
-				this.modal.load('clips', 'Insert Clips', 400);
+				this.modal.load('clips', this.lang.get('insert_clips'), 400);
 
 				this.modal.createCancelButton();
 
